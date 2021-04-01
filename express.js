0d0828c0ea1e6823b007b947ceda4d795404f60e
Marco Estrada <marco2110estrada@gmail.com>
2021-04-01 17:33:24 -0400

    intial commit

---
+++ express.js
@@ -0,0 +1,20 @@
+// //Dependecies
+// const express = require('express');
+// const apiRoutes = require('./routes/api/apiRoutes'); 
+// const htmlRoutes = require('./routes/htmlRoutes');
+// //Setting up express
+// const app = express();
+// const PORT = process.env.PORT || 8000; 
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(express.static('public'));
+
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+// //server working with localhost:3000
+// app.listen(PORT, function() {
+//     console.log(`listeng at localhost:${PORT}`);
+// })
+
---
+++ routes/indHtml.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const router = express.Router();
+// const htmlRoutes = require('./htmlRoutes');
+// const api = require('./api');
+
+
+// router.use('/api', api);
+// router.use(htmlRoutes);
+
+// module.exports = router;
