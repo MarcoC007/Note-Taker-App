404ac8be90cbebad77f79077e49362182befee7c
Marco Estrada <marco2110estrada@gmail.com>
2021-04-04 13:11:06 -0400

    commit

--- routes/api/indApi.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const apiRoutes = require('./apiRoutes');
-
-router.use('/notes', apiRoutes);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,144 +1,106 @@
-// const express = require('express');
-// const path = require('path');
-// const util = require('util');
-// const fs = require('fs');
-// const {v1: uuidv1 } = require('uuid');
+const express = require('express');
+const path = require('path');
+const util = require('util');
+const fs = require('fs');
+const {v1: uuidv1 } = require('uuid');
+const morgan = require('morgan');
 
-// // reading and writing app
-// const readFile = util.promisify(fs.readFile);
-// const writeFile = util.promisify(fs.writeFile);
-// uuidv1();
-// console.log(uuidv1());
+// reading and writing app
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+uuidv1();
+console.log(uuidv1());
 
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname + '/public')));
-
-// //HTML Routes
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/public/index.html'));
-// });
-
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/public/notes.html'));
-// });
-
-// //Api get routes
-// app.get('/api/notes', async function(req, res) {
-//     const data = await getData();
-//     res.status(200).json(data);
-// })
-
-// //Api post 
-// app.post('/api/notes', async function(req, res) {
-//     let note = req.body;
-//     const newData = await getData();
-//     newData.push({
-//         ...note,
-//         id: newData.length
-//     });
-//     await writeData(newData);
-//     return res.status(202);
-//  });
-
-//  app.delete('/api/notes:id', async function(req, res) {
-//      let noteId = Number(req.body.id);
-//      const newData = await getData()
-//      const note = newData.reduce((acc, curr) =>{
-//          if(curr.id !== noteId) acc.push(curr); 
-//              return acc;
-//      }, []);
-//      await writeData(note);
-//      return res.sendStatus(200);
-//  });
-
-// app.delete('/api/notes/:id', (req, res) => {
-
-// })
-
-//  //This function read the notes from
-//  getData = async () => {
-// try {
-//     const data = await readFile(path.join(__dirname, './db/db.json'), 'utf8');
-//     return JSON.parse(data);
-// } catch(err) {
-//     console.log(err);
-//     return err;
-// }
-//  }
-
-//  //This function write the data
-//  writeData = async (data) => {
-//      try {
-//     const update = await writeFile(path.join(__dirname, './db/db.json'), 
-//     JSON.stringify(data)
-//     );
-//     return update;
-//      } catch(err) {
-//          console.log(err);
-//          return err;
-//      }
-//  }
-
-//  //Here start the server
-//  app.listen(PORT, () => {
-//      console.log(`Server listening on: localhost:${PORT}`)
-//  })
-console.log("======================================================================");
-// //Dependecies
-// const express = require('express');
-// const path = require('path');
-// const util = require('util');
-// const {v1: v1uuid} = require('uuid');
-// const fs = require('fs');
-
-// //Reading and Writing app
-// const readFile = util.promisify(fs.readFile);
-// const writeFile = util.promisify(fs.writeFile);
-
-// const app = require();
-// const PORT = process.env.PORT || 3500;
-
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname + '/public')));
 
 
+ //This function read the notes from
+ getData = async () =>  {
+    const notes = await readFile('db/db.json', 'utf8');
+      let pNotes;
+      try {
+          pNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+          pNotes = [];
+      }
+      return pNotes;
+  }
+ 
+ 
+  //This function write the data
+  writeData = async (data) => {
+      try {
+     const update = await writeFile(path.join(__dirname, './db/db.json'), 
+     JSON.stringify(data)
+     );
+     return update;
+      } catch(err) {
+          console.log(err);
+          return err;
+      }
+  }
+ 
 
-// //HTML Routes
-// const myLogger = function (req, res, next) {
-//     console.log("Logged");
-//     next();
-// };
+//HTML Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-// console.log(myLogger);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
-
-// app.get('/', (req, res) => {
-//     res.senFile(path.join(__dirname , '/public/index.html'));
-// });
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/public/index.html'));
-// })
-
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/public/notes.html'));
-// });
-
-//Api routes
-//  getNote = () => {
-  
-//     const readNote = readFile(path.join(__dirname, 'utf8', '/db/db.json'));
-
-//     app.get('/api/notes' , (req, res) => {
-        
-//     })
-// }
-
-app.listen(PORT, (req, res) => {
-    console.log('listening on port: ' + PORT);
+//Api get routes
+app.get('/api/notes', async function(req, res) {
+    const data = await getData();
+    res.status(200).json(data);
 })
+
+//Api post 
+app.post('/api/notes', async function(req, res) {
+    let note = req.body;
+    req.body.id = uuidv1();
+    const newData = await getData();
+    newData.push({
+        ...note,
+        id: newData.length
+    });
+    await writeData(newData);
+    return res.status(201);
+ });
+
+ app.delete('/api/notes:id', function(req, res) {
+    // removeNote = () => {
+    // let noteId = req.body.id;
+    // return getData()
+    // .then((notes) => notes.filter((note) => note.id !== noteId))
+    // .then((deletedNotes) => writeData(deletedNotes));
+    // }
+    // removeNote(req.params.id)
+    // .then(() => res.json({ ok: true}))
+    // .catch((err) => res.sendStatus(200).json(err));
+    let Note = JSON.parse(fs.readFileSync(db, "utf8"));
+
+    let idDel = req.params.id;
+    
+    Note = Note.filter(currentNote => {
+        return currentNote.id !== idDel;
+    });
+
+    writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(Note), (err) => {
+        if (err) throw err;
+        res.json(Note);
+    })
+ });
+
+
+ //Here start the server
+ app.listen(PORT, () => {
+     console.log(`Server listening on: localhost:${PORT}`)
+ })
